4321e07c683ac1c8b0917445d0d559ec9ff9b78b
swj04091 <swj04091@naver.com>
2019-12-09 18:25:11 +0900

    <권혁태, 이승종> 플레이어의 랭킹 출력(오후)
    Mapper : BowlingPlayerMapper
    Service : BowlingPlayerService
    Controller : BowlingPlayerController
    Folder: ranking
    -> 개인순위를 보여주기 위한 MVC 형태 구성, ranking폴더 생성 하여 순위에 관련된 view파일 관리

---
+++ bowling/src/main/java/com/example/bowling/mapper/BowlingPlayerMapper.java
@@ -0,0 +1,8 @@
+package com.example.bowling.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BowlingPlayerMapper {
+
+}
---
+++ bowling/src/main/java/com/example/bowling/service/BowlingPlayerService.java
@@ -0,0 +1,5 @@
+package com.example.bowling.service;
+
+public interface BowlingPlayerService {
+
+}
